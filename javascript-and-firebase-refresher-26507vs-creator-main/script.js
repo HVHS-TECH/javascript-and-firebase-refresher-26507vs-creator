79abe37fa3d293e6123371514610f6227871470b
26507vs-creator <26507vs@hvhs.school.nz>
2026-02-05 13:30:38 +1300

    session 1 - added buttons

--- javascript-and-firebase-refresher-26507vs-creator-main/script.js
+++ javascript-and-firebase-refresher-26507vs-creator-main/script.js
@@ -1,6 +1,32 @@
-var messageSpace = document.getElementById("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+
 
 function swapText() {
-    
+
+    var x = document.getElementById("swapText");
+    if (x.innerHTML === "Welcome") {
+        x.innerHTML = "You pressed the button :p";
+  } else {
+        x.innerHTML = "Welcome";
+  }
 }
+
+function changeHeading(){
+    var heading = document.getElementById("changeHeading");
+    heading.innerHTML = "HOW DARE YOU!";
+}
+
+
+const input = document.getElementById('name');
+const output = document.getElementById('output');
+
+function printInput(){
+  output.innerHTML = input.value;
+}
+
+input.addEventListener("input", function () {
+  if (input.value === "") {
+    output.textContent = "Welcome";
+  } else {
+    output.textContent = `Welcome, ${input.value}!`;
+  }
+});
